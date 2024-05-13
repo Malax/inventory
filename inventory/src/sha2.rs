8dc4d052383872f2b06e86bdd3913e6c6726f600
Manuel Fuchs <manuel.fuchs@salesforce.com>
2024-05-13 10:09:56 +0200

    Fix sha2 implementations

--- inventory/src/sha2.rs
+++ inventory/src/sha2.rs
@@ -1,10 +1,13 @@
-use crate::checksum::{ChecksumSize, Digest};
-use sha2::digest::OutputSizeUser;
-use sha2::{Digest, Sha256, Sha512};
+use crate::checksum::Digest;
+use sha2::{Sha256, Sha512};
 
 impl Digest for Sha256 {
     fn name_compatible(name: &str) -> bool {
         name == "sha256"
+    }
+
+    fn length() -> usize {
+        Self::output_size()
     }
 }
 
@@ -12,13 +15,8 @@ impl Digest for Sha512 {
     fn name_compatible(name: &str) -> bool {
         name == "sha512"
     }
-}
 
-impl<T> ChecksumSize for T
-where
-    T: OutputSizeUser,
-{
-    fn checksum_size() -> usize {
+    fn length() -> usize {
         Self::output_size()
     }
 }
